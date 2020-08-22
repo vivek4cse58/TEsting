871c68eba8d95aa1d2018231ef6eba0bf4999a9a
vivek4cse58 <70068819+vivek4cse58@users.noreply.github.com>
2020-08-22 21:49:58 +0530

    Add files via upload

---
+++ Datamigration_Sprint_103.sql
@@ -0,0 +1,19 @@
+
+IF NOT EXISTS (
+  SELECT * 
+  FROM   sys.columns 
+  WHERE  object_id = OBJECT_ID('PartsOrderSubstitueMapping') 
+         AND name = 'IsActive'
+)
+Begin
+alter table PartsOrderSubstitueMapping add IsActive bit
+END
+
+
+Insert into PartsOrderSubstitueMapping 
+select Isnull(REQUESTED_PART_NO,Part_NO),QTY_ORDERED,pod.Id,'Datamigration_Sprint_103',Getutcdate(),
+Case when Isnull(pod.status,'')<>'D' then 1 else 0 end
+from PartsOrderDetail pod
+inner join PartsOrderheader poh on pod.PartsOrderHeaderId=poh.Id
+where Pending='Y' and Isnull(Part_NO,'')<>'' and 
+not exists(select 1 from PartsOrderSubstitueMapping pos where pos.PartsOrderDetailId=pod.id)
